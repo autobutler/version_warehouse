01e1a052045ba94af65ec77a128bc58d6adf5ce9
Andreas Eriksson <andreas@codered.se>
2020-11-02 13:17:41 +0100

    Tweak configs

--- config/prod.exs
+++ config/prod.exs
@@ -10,18 +10,15 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :version_warehouse, VersionWarehouseWeb.Endpoint,
-  # url: [host: "example.com", port: 80],
-  # cache_static_manifest: "priv/static/cache_manifest.json"
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "autobutler-versions.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :version_warehouse, VersionWarehouse.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: 20,
+  pool_size: 18,
   ssl: true
 
 # Do not print debug messages in production
